a6d0e7c1e1157c76e52b2ad579535c994896ef6f
mrmiguez <r.m.miguez@gmail.com>
2017-01-04 14:43:20 -0500

    'contexts sorted'

--- FlaLd.py
+++ FlaLd.py
@@ -2,91 +2,6 @@ import json
 from pyld import jsonld
 from pymods import MODS, FSUDL
 
-context = {
-            "dc": "http://purl.org/dc/elements/1.1/",
-            "dcmitype": "http://purl.org/dc/dcmitype/",
-            "dcterms": "http://purl.org/dc/terms/",
-            "dpla": "http://dp.la/terms/",
-            "edm": "http://www.europeana.eu/schemas/edm/",
-            "geo": "http://www.w3.org/2003/01/geo/wgs84_pos#",
-            "lcsh": "http://id.loc.gov/authorities/subjects/",
-            "ore": "http://www.openarchives.org/ore/terms/",
-            "skos": "http://www.w3.org/2004/02/skos/core#",
-            "xsd": "http://www.w3.org/2001/XMLSchema#",
-            "@vocab": "http://purl.org/dc/elements/1.1/",
-            "sourceResource": "dpla:SourceResource",
-            "aggregatedCHO": {
-              "@id": "edm:aggregatedCHO",
-              "@type": "@id"
-            },
-            "collection": {
-              "@id": "dcterms:isPartOf",
-              "@type": "dcmitype:Collection"
-            },
-            "extent": "dcterms:extent",
-            "temporal": {
-              "@id": "dcterms:temporal",
-              "@type": "edm:TimeSpan"
-            },
-            "spatial": {
-              "@id": "dcterms:spatial",
-              "@type": "dpla:Place"
-            },
-            "coordinates": "geo:lat_long",
-            "city": "dpla:city",
-            "county": "dpla:county",
-            "region": "dpla:region",
-            "state": "dpla:state",
-            "country": "dpla:country",
-            "stateLocatedIn": "edm:currentLocation",
-            "dataProvider": "edm:dataProvider",
-            "edmRights": "edm:rights",
-            "hasView": {
-              "@id": "edm:hasView",
-              "@type": "edm:WebResource"
-            },
-            "intermediateProvider": "dpla:intermediateProvider",
-            "isShownAt": {
-              "@id": "edm:isShownAt",
-              "@type": "@id"
-            },
-            "object": {
-              "@id": "edm:object",
-              "@type": "@id"
-            },
-            "provider": {
-              "@id": "edm:provider",
-              "@type": "edm:Agent"
-            },
-            "date": {
-              "@type": "edm:TimeSpan"
-            },
-            "begin": {
-              "@id": "edm:begin",
-              "@type": "xsd:date"
-            },
-            "end" : {
-              "@id": "edm:end",
-              "@type": "xsd:date"
-            },
-            "displayDate": "skos:prefLabel",
-            "name": "skos:prefLabel",
-            "iso639": "skos:altLabel",
-            "iso639_3": "skos:altLabel",
-            "admin": None,
-            "id": None,
-            "_id": None,
-            "ingestionSequence": None,
-            "ingestDate": None,
-            "ingestType": None,
-            "object_status": None,
-            "originalRecord": None,
-            "score": None,
-            "specType": "edm:hasType",
-            "valid_after_enrich": None,
-            "validation_message": None
-          }
-
 testData = MODS('testData/fsu_nap01-1.xml')
 docs = []
 for record in testData.record_list:
@@ -111,13 +26,14 @@ for record in testData.record_list:
                     for child in subject['children']:
                         subject_term = subject_term + '--' + child['term']
                     sourceResource['subject'].append({"@id": subject['valueURI'], "name": subject_term.strip('.,-')})
-    docs.append({"sourceResource": sourceResource})
+    docs.append({"@context": "http://api.dp.la/items/context", "sourceResource": sourceResource})
 
-#compacted = jsonld.compact(docs, context)
+#compacted = jsonld.compact(docs, "http://api.dp.la/items/context")
+#expanded = jsonld.expand(docs)
 
 print(json.dumps(docs, indent=2))
 
-"""
+""" Unimplemented writer
 with open('testData/fsu_nap01-1.jsonld', 'w') as jsonOutput:
     docs = []
     for record in testData.record_list:
