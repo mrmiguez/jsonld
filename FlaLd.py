7d8921d40384c720fbd3e9599c230b4f5ee83ecd
mrmiguez <r.m.miguez@gmail.com>
2017-01-04 11:05:04 -0500

    'more serialization'

--- FlaLd.py
+++ FlaLd.py
@@ -93,20 +93,27 @@ for record in testData.record_list:
 #    print(record) #test trace
     sourceResource = {}
     sourceResource['title'] = MODS.title_constructor(record)[0]
-    sourceResource['date'] = MODS.date_constructor(record)
-    sourceResource['subject'] = []
+    if MODS.date_constructor(record) is not None:
+        date = MODS.date_constructor(record)
+        if ' - ' in date:
+            sourceResource['date'] = { "displayDate": date, "begin": date[0:4], "end": date[-4:] }
+        else:
+            sourceResource['date'] = { "displayDate": date, "begin": date, "end": date }
+    sourceResource['type'] = MODS.type_of_resource(record)
+#    sourceResource['rights'] = #not yet implemented in pymods
     if MODS.subject(record) is not None:
+        sourceResource['subject'] = []
         for subject in MODS.subject(record):
-            print(subject) #test trace
+#            print(subject) #test trace
             if subject['authority'] is not None:
                 if 'lcsh' == subject['authority']:
                     subject_term = ""
                     for child in subject['children']:
                         subject_term = subject_term + '--' + child['term']
                     sourceResource['subject'].append({"@id": subject['valueURI'], "name": subject_term.strip('.,-')})
-    docs.append(sourceResource)
+    docs.append({"sourceResource": sourceResource})
 
-# compacted = jsonld.compact(docs, context)
+#compacted = jsonld.compact(docs, context)
 
 print(json.dumps(docs, indent=2))
 
